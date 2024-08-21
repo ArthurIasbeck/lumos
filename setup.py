4a24f48278b861108eef7c8d5abff7de341303b4
Arthur Iasbeck <arthuriasbeck@gmail.com>
2024-08-21 17:57:05 -0300

    Inclusão do setup.py.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="lumos",
+    version="0.0.1",
+    packages=find_packages(),
+    description="Pacote empregado na resolução de problemas de otimização por meio do emprego de AGs.",
+    author="Arthur Iasbeck",
+    author_email="arthuriasbeck@ufu.br",
+    url="https://github.com/ArthurIasbeck/lumos",
+)
